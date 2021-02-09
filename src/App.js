b21308edf9fb7ef2972fd02535ee8792fe7c9008
vVviewVv <59278107+vVviewVv@users.noreply.github.com>
2021-02-09 14:40:12 +0700

    Solution Solution

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { createContext, useEffect, useReducer } from "react";
 import ItemTable from "./ItemTable";
-// import { useState, useEffect } from "react";
-export const sample = createContext();
+import { useState, useEffect } from "react";
 function App() {
   //add useState for all state variables
-
-  const persons = [
-    {
-      name: "Bob",
-      gender: "male",
-      age: "50"
-    }
-  ];
-
+  const [item, setitems] = useState([]);
+  const [inputData, setInputData] = useState({ name: "", gender: "", age: 0 });
   const view = [
     {
       name: "lakkhanan issara 620610805",
@@ -24,79 +15,96 @@ function App() {
   useEffect(() => {
     const items = localStorage.getItem("items");
     // ...
+    if (items != null) {
+      setitems(JSON.parse(items));
+    }
   }, []);
 
-  function addCourse(inputData) {
-    console.log(inputData);
-    // ALL
-    const Name = [...persons, inputData];
-  }
+  useEffect(() => {
+    //add item to localStorage when item change
+    localStorage.setItem("items", JSON.stringify(item));
+  }, [item]);
 
   return (
-    <sample.Provider value={addCourse}>
-      <div className="card" style={{ width: 400 }}>
-        <div className="card-content">
-          <p className="is-4 title has-text-centered">Add Person</p>
-          <div className="field">
-            <label className="label">Name</label>
-            <input
-              className="input"
-              type="text"
-              placeholder="e.q John Smith"
-              //update related state based on event
-            >
-              {persons.name}
-            </input>
-          </div>
-
-          <div className="field">
-            <label className="label">Gender</label>
-            <select
-              className="input"
-              type="text"
-              placeholder="Please select .."
-            >
-              <option value="" disabled selected hidden>
-                -- Select Gender --
-              </option>
-              <option>Male{persons.gender}</option>
-              <option>Female{persons.gender}</option>
-            </select>
-          </div>
-
-          <div className="field">
-            <label className="label">Age</label>
-            <input className="input" type="number" placeholder="e.q 30">
-              {persons.age}
-            </input>
-          </div>
-
-          <button
-            className="button is-primary is-fullwidth"
-            onClick={addCourse}
-          >
-            Submit
-          </button>
-
-          <div className="mb-4"></div>
-
-          {/* display tables for all persons */}
-          <p className="is-4 title has-text-centered">Person List</p>
-          {/* sample table */}
-          {persons.map((item) => {
-            return (
-              <ItemTable name={item.name} gender={item.gender} age={item.age} />
-            );
-          })}
-          <p>Your name and code here</p>
-          {view.map((item) => {
-            return (
-              <ItemTable name={item.name} gender={item.gender} age={item.age} />
-            );
-          })}
+    <div className="card" style={{ width: 400 }}>
+      <div className="card-content">
+        <p className="is-4 title has-text-centered">Add Person</p>
+        <div className="field">
+          <label className="label">Name</label>
+          <input
+            id="name"
+            className="input"
+            type="text"
+            placeholder="e.q John Smith"
+            //update related state based on event
+            onChange={(e) => {
+              setInputData({ ...inputData, name: e.currentTarget.value });
+            }}
+          ></input>
         </div>
+
+        <div className="field">
+          <label className="label">Gender</label>
+          <select
+            id="gender"
+            className="input"
+            type="text"
+            placeholder="Please select .."
+            onChange={(e) => {
+              setInputData({ ...inputData, gender: e.currentTarget.value });
+            }}
+          >
+            <option value="" disabled selected hidden>
+              -- Select Gender --
+            </option>
+            <option>Male</option>
+            <option>Female</option>
+          </select>
+        </div>
+
+        <div className="field">
+          <label className="label">Age</label>
+          <input
+            id="age"
+            className="input"
+            type="number"
+            placeholder="e.q 30"
+            onChange={(e) => {
+              setInputData({ ...inputData, age: e.currentTarget.value });
+            }}
+          ></input>
+        </div>
+
+        <button
+          className="button is-primary is-fullwidth"
+          onClick={() => {
+            const newitem = [...item, inputData];
+            setitems(newitem);
+            document.querySelector("#name").value = "";
+            document.querySelector("#age").value = "";
+            document.querySelector("#gender").value = "";
+          }}
+        >
+          Submit
+        </button>
+
+        <div className="mb-4"></div>
+        {/* display tables for all persons */}
+        <p className="is-4 title has-text-centered">Person List</p>
+        {/* sample table */}
+        <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
+
+        {item.map((i) => {
+          return <ItemTable name={i.name} gender={i.gender} age={i.age} />;
+        })}
+        <p>Your name and code here</p>
+        {view.map((item) => {
+          return (
+            <ItemTable name={item.name} gender={item.gender} age={item.age} />
+          );
+        })}
       </div>
-    </sample.Provider>
+    </div>
   );
 }
 
